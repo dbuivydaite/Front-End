4ee9f24033c8afe37241c29b452b68bce29e8319
dbuivydaite <dbuivydaite@icloud.com>
2023-02-12 01:17:11 +0200

    pakeista kintamuju deklaracija

--- Savarankiški darbai/14/script.js
+++ Savarankiški darbai/14/script.js
@@ -1,18 +1,18 @@
 "use strict"
-var inputName = document.getElementById("inputName")
-var inputSurname = document.getElementById("inputSurname")
-var inputAge = document.getElementById("inputAge")
-var add = document.getElementById("add")
-var deleteFirst = document.getElementById("deleteFirst")
-var deleteLast = document.getElementById("deleteLast")
-var tbody = document.getElementById("tbody")
+const inputName = document.getElementById("inputName")
+const inputSurname = document.getElementById("inputSurname")
+const inputAge = document.getElementById("inputAge")
+const add = document.getElementById("add")
+const deleteFirst = document.getElementById("deleteFirst")
+const deleteLast = document.getElementById("deleteLast")
+const tbody = document.getElementById("tbody")
 
 add.addEventListener("click", function () {
     if (inputName.value && inputSurname.value && inputAge.value > 0) {
-        var row = tbody.insertRow(-1)
-        var cell1 = row.insertCell(0)
-        var cell2 = row.insertCell(1)
-        var cell3 = row.insertCell(2)
+        let row = tbody.insertRow(-1)
+        let cell1 = row.insertCell(0)
+        let cell2 = row.insertCell(1)
+        let cell3 = row.insertCell(2)
         cell1.innerText = inputName.value
         cell2.innerText = inputSurname.value
         cell3.innerText = inputAge.value
